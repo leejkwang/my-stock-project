163bdbfaca312dbd95e5292315c439df6d325f71
amise <mugungflower@naver.com>
2026-01-04 10:16:44 +0900

    Update date range to 2024.01.01~2025.12.31

--- my_kospi.py
+++ my_kospi.py
@@ -2,7 +2,7 @@ import FinanceDataReader as fdr
 import matplotlib.pyplot as plt
 
 # 데이터를 야후 파이낸스에서 가져옵니다 (^KS11)
-df = fdr.DataReader('^KS11', '2025-11-01', '2025-12-31')
+df = fdr.DataReader('^KS11', '2024-01-01', '2025-12-31')
 
 # 데이터 저장 및 그래프 설정
 df.to_csv('kospi_data.csv')
